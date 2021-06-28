315e8a840ba1e3b8767361408f2e4062c944aa53
smilu97 <smilup2244@gmail.com>
2021-06-28 21:17:44 +0900

    Modified CachePolicy.h

--- buffer/CachePolicy.h
+++ buffer/CachePolicy.h
@@ -6,18 +6,20 @@
 #define KEYVALUE_CACHEPOLICY_H
 
 #include <cstdint>
+#include <optional>
 #include <vector>
 
 class CachePolicy {
 private:
     uint32_t _sz;
-    std::vector<uint32_t> _rooms;
+    std::vector< std::optional<uint32_t> > _rooms;
 public:
-    CachePolicy(uint32_t sz): _sz(sz), _rooms(sz) {}
-    uint32_t Size() { return _sz; }
-    uint32_t Get(uint32_t index) { return _rooms[index]; }
-    void Set(uint32_t index, uint32_t value) { _rooms[index] = value; }
-    virtual std::pair<uint32_t, uint32_t> push(uint32_t n) = 0;
+    explicit CachePolicy(uint32_t sz): _sz(sz), _rooms(sz) {}
+    [[nodiscard]] uint32_t Size() const { return _sz; }
+    std::optional<uint32_t> GetRoom(uint32_t index) { return _rooms[index]; }
+    void SetRoom(uint32_t index, uint32_t value) { _rooms[index] = value; }
+    virtual std::optional<uint32_t> Get(uint32_t n) = 0;
+    virtual std::pair<uint32_t, std::optional<uint32_t>> Load(uint32_t n) = 0;
 };
 
 #endif //KEYVALUE_CACHEPOLICY_H
